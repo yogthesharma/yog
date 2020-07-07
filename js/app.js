086e7ac3e989571299cfdadf457495bcb11397e3
Yog Sharma <yogthesharma@gmail.com>
2020-07-07 22:38:55 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// importing
+import ScrollReveal from "scrollreveal";
+
+// toggler menu
+
+const togglerDiv = document.getElementById("togglerDiv");
+const togglerBtn = document.getElementById("togglerBtn");
+const links = document.querySelector("#togglerDiv ul");
+
+let temp = false;
+togglerBtn.addEventListener("click", () => {
+  //   alert();
+
+  if (!temp) {
+    togglerDiv.style.transform = "translateY(48px)";
+    setTimeout(() => {
+      links.style.opacity = 1;
+      links.style.transform = "translateY(0)";
+    }, 500);
+    return (temp = true);
+  } else if (temp) {
+    togglerDiv.style.transform = "translateY(-500px)";
+    links.style.opacity = 0;
+    links.style.transform = "translateY(-200)";
+    return (temp = false);
+  }
+});
+
+const sendBtn = document.getElementById("sendBtnId");
+
+sendBtn.addEventListener("click", () => {
+  alert();
+});
